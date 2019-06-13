a1aa3539e00560270f1107e54df48b55866a9f83
xdedzl <xdedzl@163.com>
2019-06-13 19:27:57 +0800

    ui

--- XFramework/Assets/XFrameworkDemo/UI/Scripts/CreatePanel.cs
+++ XFramework/Assets/XFrameworkDemo/UI/Scripts/CreatePanel.cs
@@ -3,20 +3,17 @@ using DG.Tweening;
 using System.Threading.Tasks;
 using System;
 using XFramework.UI;
+using UnityEngine.UI;
 
 /// <summary>
 /// 创建单位按钮
 /// </summary>
 public class CreatePanel : BasePanel {
 
-    [HideInInspector] public ButtonExt groupBtn;        // 五群按钮
-    [HideInInspector] public ButtonExt teamBtn;         // 十八队按钮
-    [HideInInspector] public ButtonExt commandPostBtn;  // 三所按钮
+    [HideInInspector] public Button groupBtn;        // 五群按钮
+    [HideInInspector] public Button teamBtn;         // 十八队按钮
+    [HideInInspector] public Button commandPostBtn;  // 三所按钮
      
-    private RectTransform groupRect;
-    private RectTransform teamRect;
-    private RectTransform comandPostRect;
-
     private const float groupHeight = 420;
     private const float teamHeight = 533;
     private const float comandPosHeight = 336;
@@ -27,22 +24,14 @@ public class CreatePanel : BasePanel {
     {
         Level = 2;
         // 按钮赋值
-        groupBtn = ((this["GroupBtn"] as GUButton).button) as ButtonExt;
-        teamBtn = ((this["TeamBtn"] as GUButton).button) as ButtonExt;
-        commandPostBtn = ((this["CommandPostBtn"] as GUButton).button) as ButtonExt;
-
-        groupRect = groupBtn.GetComponent<RectTransform>();
-        teamRect = teamBtn.GetComponent<RectTransform>();
-        comandPostRect = commandPostBtn.GetComponent<RectTransform>();
+        groupBtn = ((this["GroupBtn"] as GUButton).button) as Button;
+        teamBtn = ((this["TeamBtn"] as GUButton).button) as Button;
+        commandPostBtn = ((this["CommandPostBtn"] as GUButton).button) as Button;
 
         // 注册鼠标点击事件
         groupBtn.onClick.AddListener(() => { OnClick(UIName.Group); });
         teamBtn.onClick.AddListener(() => { OnClick(UIName.Team); });
         commandPostBtn.onClick.AddListener(() => { OnClick(UIName.CommandPost); });
-
-        groupBtn.AddNormal(OutButton);
-        teamBtn.AddNormal(OutButton);
-        commandPostBtn.AddNormal(OutButton);
     }
 
     /// <summary>
@@ -91,28 +80,5 @@ public class CreatePanel : BasePanel {
                 break;
         }
         rect.sizeDelta = rectSize;
-    }
-
-    private void InButton(RectTransform rectTran, string str_1,string str_2 = "") => DelayIn(rectTran, str_1,str_2);
-    private void OutButton() => DelayOut();
-
-    /// <summary>
-    /// 延迟播放显示动画，延迟时间跟按钮变大时间一样
-    /// </summary>
-    private async void DelayIn(RectTransform rectTran, string str_1, string str_2)
-    {
-        await Task.Delay(TimeSpan.FromSeconds(0.2f));
-        if (rectTran.GetComponent<ButtonExt>().CurrentBtnState == 1)
-        {
-            float y = rectTran.position.y;
-        }
-    }
-
-    /// <summary>
-    /// 延迟播放消失动画
-    /// </summary>
-    private async void DelayOut()
-    {
-        await Task.Delay(TimeSpan.FromSeconds(0.2f));
     }
 }
