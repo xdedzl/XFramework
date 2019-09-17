5ccef98099a2f6b15bd3c8b33356e6bbfd826c95
薛定谔的蟑螂 <xdedzl@163.com>
2019-09-17 16:34:18 +0800

    新增枚举转自定义字符串的特性

---
+++ XFramework/Assets/XFramework/Core/Runtime/Tools/Attribute/EnumStrAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XFramework
+{
+    /// <summary>
+    /// 为枚举提供自定义字符串,结合Utility.Enum.ToCustomStrs使用
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class EnumStrAttribute : Attribute
+    {
+        public string str;
+        public EnumStrAttribute(string str)
+        {
+            this.str = str;
+        }
+    }
+}
