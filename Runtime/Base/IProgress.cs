daaf81b926cf9f00e559bbd97ef9d7dc11e3493f
薛定谔的蟑螂 <xdedzl@163.com>
2019-12-04 17:40:05 +0800

    资源加载底层的进度修改

--- Runtime/Base/IProgress.cs
+++ Runtime/Base/IProgress.cs
@@ -15,4 +15,90 @@
 
         public float Progress => 1;
     }
+
+    public class MultiProgress : IProgress
+    {
+        private IProgress[] progresses;
+
+        public MultiProgress(IProgress[] progresses)
+        {
+            this.progresses = progresses;
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                foreach (var item in progresses)
+                {
+                    if (!item.IsDone)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public float Progress
+        {
+            get
+            {
+                float p = 0;
+                foreach (var item in progresses)
+                {
+                    p += item.Progress;
+                }
+                return p / progresses.Length;
+            }
+        }
+    }
+
+    public class DynamicMultiProgress : IProgress
+    {
+        private int index = 0;
+        private IProgress[] progresses;
+
+        public DynamicMultiProgress(int count)
+        {
+            progresses = new IProgress[count];
+        }
+
+        public void Add(IProgress progress)
+        {
+            progresses[index] = progress;
+            index++;
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                foreach (var item in progresses)
+                {
+                    if (item == null || !item.IsDone)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public float Progress
+        {
+            get
+            {
+                float p = 0;
+                foreach (var item in progresses)
+                {
+                    if (item != null)
+                    {
+                        p += item.Progress;
+                    }
+                }
+                return p / progresses.Length;
+            }
+        }
+    }
 }
