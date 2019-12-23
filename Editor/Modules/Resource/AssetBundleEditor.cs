79f336de3946e03df9664f7c07366bc47d4cec55
薛定谔的蟑螂 <xdedzl@163.com>
2019-12-23 15:53:24 +0800

    打包工具以及资源加载的小调整

--- Editor/Modules/Resource/AssetBundleEditor.cs
+++ Editor/Modules/Resource/AssetBundleEditor.cs
@@ -7,6 +7,14 @@ using UnityEngine;
 
 namespace XFramework.Editor
 {
+    public enum PackOption
+    {
+        AllFiles,       // 所有文件一个包 
+        TopDirectiony,  // 一级子文件夹单独打包
+        AllDirectiony,  // 所有子文件夹单独打包
+        TopFileOnly,    // 只打包当前文件夹的文件
+    }
+
     /// <summary>
     /// AssetBundle窗口
     /// </summary>
@@ -20,16 +28,7 @@ namespace XFramework.Editor
             BuildProject,
         }
 
-        private enum PackOption
-        {
-            AllFiles,       // 所有文件一个包 
-            TopDirectiony,  // 一级子文件夹单独打包
-            AllDirectiony,  // 所有子文件夹单独打包
-            TopFileOnly,    // 只打包当前文件夹的文件
-        }
-
-
-        [MenuItem("XFramework/Resource/AssetBundleWindow")]
+        [MenuItem("SFramework/Resource/AssetBundleWindow")]
         static void OpenWindow()
         {
             var window = GetWindow(typeof(AssetBundleEditor));
@@ -81,77 +80,11 @@ namespace XFramework.Editor
         #region 
 
         /// <summary>
-        /// 标记文件
-        /// </summary>
-        /// <param name="path"></param>
-        /// <param name="packOption"></param>
-        private static void MarkDirectory(DirectoryInfo dirInfo, PackOption packOption, ref List<AssetBundleBuild> builds)
-        {
-            FileInfo[] files = null;
-            DirectoryInfo[] subDirectory;
-            switch (packOption)
-            {
-                case PackOption.AllFiles:
-                    files = dirInfo.GetFiles("*", SearchOption.AllDirectories);        // 取出所有文件
-                    break;
-                case PackOption.TopDirectiony:
-                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);      // 取出第一层文件
-                    subDirectory = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
-                    foreach (var item in subDirectory)
-                    {
-                        MarkDirectory(item, PackOption.AllFiles, ref builds);
-                    }
-                    break;
-                case PackOption.AllDirectiony:
-                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);      // 取出第一层文件
-                    subDirectory = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
-                    foreach (var item in subDirectory)
-                    {
-                        MarkDirectory(item, PackOption.AllDirectiony, ref builds);
-                    }
-                    break;
-                case PackOption.TopFileOnly:
-                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);        // 取出所有文件
-                    break;
-            }
-
-            // 添加AssetBundleBuild
-            List<string> assetNames = new List<string>();
-
-            int total = files.Length;
-            string abName = dirInfo.FullName.Substring(dirInfo.FullName.IndexOf("Assets", StringComparison.Ordinal));
-            for (int i = 0; i < total; i++)
-            {
-                var fileInfo = files[i];
-
-                if (fileInfo.Name.EndsWith(".meta")) continue;
-
-                EditorUtility.DisplayProgressBar(dirInfo.Name, $"正在标记资源{fileInfo.Name}...", (float)i / total);
-
-                string filePath = fileInfo.FullName.Substring(fileInfo.FullName.IndexOf("Assets", StringComparison.Ordinal));       // 获取 "Assets"目录起的 文件名, 可不用转 "\\"
-
-                assetNames.Add(filePath);
-            }
-
-            if (assetNames.Count > 0)
-            {
-                builds.Add(new AssetBundleBuild()
-                {
-                    assetBundleName = abName,
-                    assetBundleVariant = "ab",
-                    assetNames = assetNames.ToArray()
-                });
-            }
-
-            EditorUtility.ClearProgressBar();
-        }
-
-        /// <summary>
         /// 将unity依赖转为自己的
         /// </summary>
         /// <param name="mainfest"></param>
         /// <returns></returns>
-        private static DependenciesData GenerateDependence(AssetBundleManifest mainfest)
+        public static DependenciesData GenerateDependence(AssetBundleManifest mainfest)
         {
             string[] abNames = mainfest.GetAllAssetBundles();
 
---
+++ Editor/Modules/Resource/AssetBundleUtility.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using static XFramework.Editor.AssetBundleEditor;
+
+namespace XFramework.Editor
+{
+    public static class AssetBundleUtility
+    {
+        /// <summary>
+        /// 标记文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="packOption"></param>
+        public static List<AssetBundleBuild> MarkDirectory(DirectoryInfo dirInfo, PackOption packOption, bool isRootNameEqualAsset = true)
+        {
+            List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
+            if (isRootNameEqualAsset)
+            {
+                MarkDirectory(dirInfo, packOption, "Assets", ref builds);
+            }
+            else
+            {
+                MarkDirectory(dirInfo, packOption, dirInfo.Name, ref builds);
+            }
+
+            return builds;
+        }
+
+        /// <summary>
+        /// 标记文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="packOption"></param>
+        private static void MarkDirectory(DirectoryInfo dirInfo, PackOption packOption, string markRootName, ref List<AssetBundleBuild> builds)
+        {
+            FileInfo[] files = null;
+            DirectoryInfo[] subDirectory;
+            switch (packOption)
+            {
+                case PackOption.AllFiles:
+                    files = dirInfo.GetFiles("*", SearchOption.AllDirectories);        // 取出所有文件
+                    break;
+                case PackOption.TopDirectiony:
+                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);      // 取出第一层文件
+                    subDirectory = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
+                    foreach (var item in subDirectory)
+                    {
+                        MarkDirectory(item, PackOption.AllFiles, markRootName, ref builds);
+                    }
+                    break;
+                case PackOption.AllDirectiony:
+                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);      // 取出第一层文件
+                    subDirectory = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly);
+                    foreach (var item in subDirectory)
+                    {
+                        MarkDirectory(item, PackOption.AllDirectiony, markRootName, ref builds);
+                    }
+                    break;
+                case PackOption.TopFileOnly:
+                    files = dirInfo.GetFiles("*", SearchOption.TopDirectoryOnly);        // 取出所有文件
+                    break;
+            }
+
+            // 添加AssetBundleBuild
+            List<string> assetNames = new List<string>();
+
+            int total = files.Length;
+            string abName = dirInfo.FullName.Substring(dirInfo.FullName.IndexOf(markRootName, StringComparison.Ordinal));
+            for (int i = 0; i < total; i++)
+            {
+                var fileInfo = files[i];
+
+                if (fileInfo.Name.EndsWith(".meta")) continue;
+
+                EditorUtility.DisplayProgressBar(dirInfo.Name, $"正在标记资源{fileInfo.Name}...", (float)i / total);
+
+                string filePath = fileInfo.FullName.Substring(fileInfo.FullName.IndexOf("Assets", StringComparison.Ordinal));       // 获取 "Assets"目录起的 文件名, 可不用转 "\\"
+
+                assetNames.Add(filePath);
+            }
+
+            if (assetNames.Count > 0)
+            {
+                builds.Add(new AssetBundleBuild()
+                {
+                    assetBundleName = abName,
+                    assetBundleVariant = "ab",
+                    assetNames = assetNames.ToArray()
+                });
+            }
+
+            EditorUtility.ClearProgressBar();
+        }
+    }
+}
