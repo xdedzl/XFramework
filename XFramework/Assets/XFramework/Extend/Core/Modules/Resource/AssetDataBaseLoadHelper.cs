406c696b6fc1f4287e28294d463153070703b7c1
薛定谔的蟑螂 <xdedzl@163.com>
2020-04-02 15:05:22 +0800

    dev: 升级资源管理，支持异步的LoadAll以及子文件夹的ab包加载

--- XFramework/Assets/XFramework/Extend/Core/Modules/Resource/AssetDataBaseLoadHelper.cs
+++ XFramework/Assets/XFramework/Extend/Core/Modules/Resource/AssetDataBaseLoadHelper.cs
@@ -16,18 +16,48 @@ namespace XFramework.Resource
             m_AssetPath = Application.dataPath.Replace("/Assets", "");
         }
 
+        /// <summary>
+        /// 资源路径
+        /// </summary>
         public string AssetPath => m_AssetPath;
 
+        /// <summary>
+        /// 同步加载资源
+        /// </summary>
+        /// <typeparam name="T">资源类型</typeparam>
+        /// <param name="assetName">资源名称</param>
+        /// <returns>资源</returns>
         public T Load<T>(string assetName) where T : UnityEngine.Object
         {
             return AssetDatabase.LoadAssetAtPath<T>(assetName);
         }
 
-        public T[] LoadAll<T>(string path) where T : UnityEngine.Object
+        /// <summary>
+        /// 同步加载一组资源
+        /// </summary>
+        /// <typeparam name="T">资源类型</typeparam>
+        /// <param name="path">资源路径</param>
+        /// <param name="isTopOnly">是否是仅加载本层级的资源</param>
+        /// <returns>资源</returns>
+        public T[] LoadAll<T>(string path, bool isTopOnly = true) where T : UnityEngine.Object
         {
-            return LoadAllWithADB<T>(path);
+            if (isTopOnly)
+            {
+                return LoadAllWithADB<T>(path, SearchOption.TopDirectoryOnly);
+            }
+            else
+            {
+                return LoadAllWithADB<T>(path, SearchOption.AllDirectories);
+            }
         }
 
+        /// <summary>
+        /// 效果同Load<T>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="assetName"></param>
+        /// <param name="callback"></param>
+        /// <returns></returns>
         public IProgress LoadAsync<T>(string assetName, System.Action<T> callback) where T : UnityEngine.Object
         {
             T obj = AssetDatabase.LoadAssetAtPath<T>(assetName);
@@ -36,9 +66,23 @@ namespace XFramework.Resource
         }
 
         /// <summary>
+        /// 同步加载一组资源
+        /// </summary>
+        /// <typeparam name="T">资源类型</typeparam>
+        /// <param name="path">资源路径</param>
+        /// <param name="isTopOnly">是否是仅加载本层级的资源</param>
+        /// <returns>资源</returns>
+        public IProgress LoadAllSync<T>(string path, bool isTopOnly, System.Action<T[]> callback) where T : UnityEngine.Object
+        {
+            var assets = LoadAll<T>(path, isTopOnly);
+            callback.Invoke(assets);
+            return new DefaultProgress();
+        }
+
+        /// <summary>
         /// 加载一个文件夹下的所有资源
         /// </summary>
-        private T[] LoadAllWithADB<T>(string path, SearchOption searchOption = SearchOption.TopDirectoryOnly) where T : Object
+        private T[] LoadAllWithADB<T>(string path, SearchOption searchOption) where T : Object
         {
             List<T> objs = new List<T>();
             DirectoryInfo info = new DirectoryInfo(Application.dataPath.Replace("Assets", "") + path);
