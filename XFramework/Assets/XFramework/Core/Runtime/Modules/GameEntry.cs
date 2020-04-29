c5ada1a6f040fc395c36f6a0663634ae2f21a5d5
薛定谔的蟑螂 <xdedzl@163.com>
2020-04-29 11:33:41 +0800

    dev：依赖模块的卸载

--- XFramework/Assets/XFramework/Core/Runtime/Modules/GameEntry.cs
+++ XFramework/Assets/XFramework/Core/Runtime/Modules/GameEntry.cs
@@ -10,11 +10,11 @@ namespace XFramework
     public static class GameEntry
     {
         // 当前已加载的模块
-        private static LinkedList<IGameModule> m_GameModules = new LinkedList<IGameModule>();
+        private static readonly LinkedList<IGameModule> m_GameModules = new LinkedList<IGameModule>();
         private static LinkedListNode<IGameModule> m_CurrentModule;
 
         // 模块依赖关系 key：模块, Value：依赖的模块
-        private static Dictionary<string, List<string>> m_Dependence = new Dictionary<string, List<string>>();
+        private static readonly Dictionary<string, List<Type>> m_DependenceDic = new Dictionary<string, List<Type>>();
 
         /// <summary>
         /// 每帧运行
@@ -24,11 +24,10 @@ namespace XFramework
         public static void ModuleUpdate(float elapseSeconds, float realElapseSeconds)
         {
             m_CurrentModule = m_GameModules.First;
-            m_CurrentModule.Value.Update(elapseSeconds, realElapseSeconds);
-            while (m_CurrentModule.Next != null)
+            while (m_CurrentModule != null)
             {
-                m_CurrentModule = m_CurrentModule.Next;
                 m_CurrentModule.Value.Update(elapseSeconds, realElapseSeconds);
+                m_CurrentModule = m_CurrentModule.Next;
             }
         }
 
@@ -72,104 +71,19 @@ namespace XFramework
         /// <returns>模块</returns>
         public static IGameModule AddModule(Type moduleType, params object[] args)
         {
-            foreach (var module in m_GameModules)
+            foreach (var item in m_GameModules)
             {
-                if (module.GetType() == moduleType)
+                if (item.GetType() == moduleType)
                 {
-                    return module;
-                }
-            }
-            return CreateModule(moduleType, args);
-        }
-
-        /// <summary>
-        /// 关闭一个模块
-        /// </summary>
-        /// <typeparam name="T">模块类型</typeparam>
-        /// <param name="shutdownAllDependentModule">是否卸载其依赖的模块（当没有其它模块也依赖它时）</param>
-        public static void ShutdownModule<T>(bool shutdownAllDependentModule = false) where T : IGameModule
-        {
-            Type moduleType = typeof(T);
-            foreach (var item in m_Dependence)
-            {
-                if (item.Value.Contains(moduleType.Name))
-                {
-                    throw new FrameworkException($"[Module] {item.Key}依赖于{moduleType.Name}, 请检查卸载时机或顺序");
-                }
-            }
-            IGameModule gameModule = null;
-            foreach (var module in m_GameModules)
-            {
-                if (module.GetType() == moduleType)
-                {
-                    gameModule = module;
-                    break;
+                    return item;
                 }
             }
 
-            if (gameModule != null)
-            {
-                gameModule.Shutdown();
-            }
-            m_GameModules.Remove(gameModule);
+            IGameModule module = CreateModule(moduleType, args);
 
-            if(m_Dependence.TryGetValue(moduleType.Name, out List<string> dependentModules))
-            {
-                m_Dependence.Remove(moduleType.Name);
+            // 将模块添加到链表中
 
-                if (shutdownAllDependentModule)
-                {
-                    // 卸载依赖模块
-                    foreach (var dependentModule in dependentModules)
-                    {
-                        // TODO
-                        //bool canShutdown = true;
-                        //foreach (var item in m_Dependence.Values)
-                        //{
-                        //    if (item.Contains(dependentModule))
-                        //    {
-                        //        canShutdown = false;
-                        //        break;
-                        //    }
-                        //}
-                        //if(canShutdown.)
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// 创建一个模块
-        /// </summary>
-        /// <param name="moduleType">模块类型</param>
-        /// <param name="args">模块启动参数</param>
-        /// <returns>模块</returns>
-        private static IGameModule CreateModule(Type moduleType, params object[] args)
-        {
-            // 加载依赖模块
-            var dependenceModules = moduleType.GetCustomAttributes<DependenceModuleAttribute>();
-            if (dependenceModules != null)
-            {
-                if (!m_Dependence.TryGetValue(moduleType.Name, out List<string> modules))
-                {
-                    modules = new List<string>();
-                    m_Dependence.Add(moduleType.Name, modules);
-                }
-                foreach (var item in dependenceModules)
-                {
-                    modules.Add(item.moduleType.Name);
-                    AddModule(item.moduleType, item.args);
-                }
-            }
-
-            // 加载模块
-            IGameModule module = (IGameModule)Activator.CreateInstance(moduleType, args);
-            if (module == null)
-            {
-                throw new Exception(moduleType.Name + " is not a module");
-            }
-
-            LinkedListNode<IGameModule> current = m_GameModules.First;
+            LinkedListNode < IGameModule > current = m_GameModules.First;
             while (current != null)
             {
                 if (module.Priority > current.Value.Priority)
@@ -193,6 +107,124 @@ namespace XFramework
         }
 
         /// <summary>
+        /// 创建一个模块
+        /// </summary>
+        /// <param name="moduleType">模块类型</param>
+        /// <param name="args">模块启动参数</param>
+        /// <returns>模块</returns>
+        private static IGameModule CreateModule(Type moduleType, params object[] args)
+        {
+            // 加载依赖模块
+            var dependenceModules = moduleType.GetCustomAttributes<DependenceModuleAttribute>();
+            if (dependenceModules != null)
+            {
+                if (!m_DependenceDic.TryGetValue(moduleType.Name, out List<Type> modules))
+                {
+                    modules = new List<Type>();
+                    m_DependenceDic.Add(moduleType.Name, modules);
+                }
+                foreach (var item in dependenceModules)
+                {
+                    modules.Add(item.moduleType);
+                    AddModule(item.moduleType, item.args);
+                }
+            }
+
+            // 加载模块
+            IGameModule module = (IGameModule)Activator.CreateInstance(moduleType, args);
+            if (module == null)
+            {
+                throw new Exception(moduleType.Name + " is not a module");
+            }
+
+            return module;
+        }
+
+        /// <summary>
+        /// 关闭一个模块
+        /// </summary>
+        /// <typeparam name="T">模块类型</typeparam>
+        /// <param name="shutdownAllDependentModule">是否卸载其依赖的模块（当没有其它模块也依赖它时）</param>
+        public static void ShutdownModule<T>(bool shutdownAllDependentModule = false) where T : IGameModule
+        {
+            Type moduleType = typeof(T);
+
+            ShutdownModule(moduleType, shutdownAllDependentModule);
+        }
+
+        /// <summary>
+        /// 关闭一个模块
+        /// </summary>
+        /// <param name="moduleType">模块类型</typeparam>
+        /// <param name="shutdownAllDependentModule">是否卸载其依赖的模块（当没有其它模块也依赖它时）</param>
+        public static void ShutdownModule(Type moduleType, bool shutdownAllDependentModule = false)
+        {
+            CheckShutdownSafe(moduleType);
+
+            IGameModule gameModule = null;
+            foreach (var module in m_GameModules)
+            {
+                if (module.GetType() == moduleType)
+                {
+                    gameModule = module;
+                    gameModule.Shutdown();
+                    break;
+                }
+            }
+
+            m_GameModules.Remove(gameModule);
+
+            if (m_DependenceDic.TryGetValue(moduleType.Name, out List<Type> dependentModules))
+            {
+                m_DependenceDic.Remove(moduleType.Name);
+
+                if (shutdownAllDependentModule)
+                {
+                    // 卸载依赖模块
+                    foreach (var dependentModule in dependentModules)
+                    {
+                        if (!HaveModuleDependent(dependentModule))
+                        {
+                            ShutdownModule(dependentModule, shutdownAllDependentModule);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 检测删除对应模块安全性
+        /// </summary>
+        /// <param name="moduleType"></param>
+        private static void CheckShutdownSafe(Type moduleType)
+        {
+            foreach (var item in m_DependenceDic)
+            {
+                if (item.Value.Contains(moduleType))
+                {
+                    throw new FrameworkException($"[Module] {item.Key}依赖于{moduleType.Name}, 请检查卸载时机或顺序");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 是否还有别的模块依赖于此模块
+        /// </summary>
+        /// <param name="moduleType">模块类型</param>
+        /// <returns></returns>
+        private static bool HaveModuleDependent(Type moduleType)
+        {
+            foreach (var item in m_DependenceDic)
+            {
+                if (item.Value.Contains(moduleType))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
         /// 卸载当前已加载的所有模块
         /// </summary>
         public static void CleraAllModule()
@@ -201,6 +233,9 @@ namespace XFramework
             {
                 item.Shutdown();
             }
+            m_GameModules.Clear();
+            m_CurrentModule = null;
+            m_DependenceDic.Clear();
         }
     }
 }
