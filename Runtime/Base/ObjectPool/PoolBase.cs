ff0e5d01981f4f7dcbe5fa046ed6a82a028314aa
xdedzl <xdedzl@163.com>
2020-05-01 16:50:11 +0800

    dev：改写对象池

---
+++ Runtime/Base/ObjectPool/PoolBase.cs
@@ -0,0 +1,17 @@
+﻿namespace XFramework.Pool
+{
+    public abstract class PoolBase
+    {
+        public abstract int CurrentCount { get; }
+
+        /// <summary>
+        /// 回收所有对象
+        /// </summary>
+        public abstract void RecycleAllObj();
+
+        /// <summary>
+        /// 销毁对象池
+        /// </summary>
+        public abstract void OnDestroy();
+    }
+}
