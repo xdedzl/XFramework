f0864e9f8a60861b2c55421a68bcb67db29785e4
xdedzl <xdedzl@163.com>
2020-05-31 09:51:44 +0800

    dev：流程新增子流程，GameEntry对GameModuleBase的支持

--- Runtime/Base/Procedure/ProcedureBase.cs
+++ Runtime/Base/Procedure/ProcedureBase.cs
@@ -1,4 +1,5 @@
-﻿using XFramework.Fsm;
+﻿using System.Collections.Generic;
+using XFramework.Fsm;
 
 namespace XFramework
 {
@@ -7,9 +8,51 @@ namespace XFramework
     /// </summary>
     public abstract class ProcedureBase : FsmState 
     {
+        private List<SubProcedureBase> m_subProcedureBases;
+        private SubProcedureBase m_currentSubProcedure;
+
+        public SubProcedureBase CurrentSubProcedure
+        {
+            get
+            {
+                return m_currentSubProcedure;
+            }
+        }
+
         public virtual void OnEnter(ProcedureBase preProcedure)
         {
 
         }
+
+        public override void OnUpdate()
+        {
+            m_currentSubProcedure?.OnUpdate();
+        }
+
+        public void ChangeSubProcedure<T>(params object[] args) where T : SubProcedureBase, new()
+        {
+            m_subProcedureBases = m_subProcedureBases ?? new List<SubProcedureBase>();
+
+            m_currentSubProcedure?.OnExit();
+
+            foreach (var item in m_subProcedureBases)
+            {
+                if(item.GetType() == typeof(T))
+                {
+                    m_currentSubProcedure = item;
+                    m_currentSubProcedure.OnEnter(args);
+                    return;
+                }
+            }
+
+            m_currentSubProcedure = new T();
+            m_currentSubProcedure.OnEnter(args);
+            m_subProcedureBases.Add(m_currentSubProcedure);
+        }
     }
+
+    /// <summary>
+    /// 子流程基类
+    /// </summary>
+    public abstract class SubProcedureBase : FsmState { }
 }
