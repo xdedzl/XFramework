c6cedafd8e8b827c0c811831f2a28d792e0735fc
huangankai <huangankai@corp.netease.com>
2021-08-11 20:11:26 +0800

    dev 新增一些常用路径

--- XFramework/Assets/XFramework/Core/Runtime/Base/XApplication.cs
+++ XFramework/Assets/XFramework/Core/Runtime/Base/XApplication.cs
@@ -1,9 +1,11 @@
 using System.IO;
+using UnityEngine;
 
 namespace XFramework
 {
     public static class XApplication
     {
+        public static readonly string dataPath;
         public static string CachePath
         {
             get
@@ -12,6 +14,34 @@ namespace XFramework
                 if (!Directory.Exists(path))
                     Directory.CreateDirectory(path);
                 return path;
+            }
+        }
+
+        public static string localDataPath
+        {
+            get
+            {
+                return $"{dataPath}/LocalData";
+            }
+        }
+
+        public static string configPath 
+        {
+            get
+            {
+                return $"{dataPath}/Configs";
+            }
+        }
+
+        static XApplication()
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                dataPath = System.Environment.CurrentDirectory;
+            }
+            else
+            {
+                dataPath = Application.persistentDataPath;
             }
         }
     }
