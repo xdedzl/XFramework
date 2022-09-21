080651819169250db482f2b8116c8a9fc7d2c600
Ankai Huang <xdedzl@163.com>
2022-09-21 22:31:50 +0800

    XUI

---
+++ Runtime/Modules/UI/Core/GUI/XTMPText.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+
+namespace XFramework.UI
+{
+    [UnityEngine.RequireComponent(typeof(TextMeshProUGUI))]
+    public class XTMPText : XUIBase
+    {
+        public TextMeshProUGUI text;
+
+        private void Reset()
+        {
+            text = GetComponent<TextMeshProUGUI>();
+        }
+    }
+}
