fb836a20fc05cbec33f273567fe6e9cf753a6df6
Ankai Huang <xdedzl@163.com>
2025-05-07 11:17:12 +0800

    some uitls

---
+++ Core/Utility/Utility.Gizmos.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+
+namespace XFramework
+{
+    public partial class UUtility
+    {
+        /// <summary>
+        /// UI相关工具
+        /// </summary>
+        public class Gizmos
+        {
+            /// <summary>
+            /// 绘制扇形
+            /// </summary>
+            public static void DrawWireSector(Vector3 origin, Vector3 direction, float radius, float angle)
+            {
+                DrawWireSector(origin, direction, radius, angle, Vector3.up);
+            }
+            
+
+            public static void DrawWireSector(Vector3 origin, Vector3 direction, float radius, float angle, Vector3 axis)
+            {
+                Vector3 leftdir = Quaternion.AngleAxis(-angle / 2, axis) * direction;
+                Vector3 rightdir = Quaternion.AngleAxis(angle / 2, axis) * direction;
+
+                Vector3 currentP = origin + leftdir * radius;
+                Vector3 oldP;
+                if (angle != 360)
+                {
+                    UnityEngine.Gizmos.DrawLine(origin, currentP);
+                }
+                for (int i = 0; i < angle / 10; i++)
+                {
+                    Vector3 dir = Quaternion.AngleAxis(10 * i, axis) * leftdir;
+                    oldP = currentP;
+                    currentP = origin + dir * radius;
+                    UnityEngine.Gizmos.DrawLine(oldP, currentP);
+                }
+                oldP = currentP;
+                currentP = origin + rightdir * radius;
+                UnityEngine.Gizmos.DrawLine(oldP, currentP);
+                if (angle != 360)
+                {
+                    UnityEngine.Gizmos.DrawLine(currentP, origin);
+                }
+
+            }
+
+            public static Mesh SemicircleMesh(float radius, int angle, Vector3 axis)
+            {
+                Vector3 leftdir = Quaternion.AngleAxis(-angle / 2, axis) * Vector3.forward;
+                Vector3 rightdir = Quaternion.AngleAxis(angle / 2, axis) * Vector3.forward;
+                int pcount = angle / 10;
+                //顶点
+                Vector3[] vertexs = new Vector3[3 + pcount];
+                vertexs[0] = Vector3.zero;
+                int index = 1;
+                vertexs[index] = leftdir * radius;
+                index++;
+                for (int i = 0; i < pcount; i++)
+                {
+                    Vector3 dir = Quaternion.AngleAxis(10 * i, axis) * leftdir;
+                    vertexs[index] = dir * radius;
+                    index++;
+                }
+                vertexs[index] = rightdir * radius;
+                //三角面
+                int[] triangles = new int[3 * (1 + pcount)];
+                for (int i = 0; i < 1 + pcount; i++)
+                {
+                    triangles[3 * i] = 0;
+                    triangles[3 * i + 1] = i + 1;
+                    triangles[3 * i + 2] = i + 2;
+                }
+
+                Mesh mesh = new Mesh();
+                mesh.vertices = vertexs;
+                mesh.triangles = triangles;
+                mesh.RecalculateNormals();
+                return mesh;
+            }
+        }
+    }
+}
---
+++ Core/Utility/Utility.Json.cs
@@ -0,0 +1,68 @@
+﻿//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace XFramework
+{
+    public partial class Utility
+    {
+        public static class Json
+        {
+            /// <summary>
+            /// 序列化多态数组
+            /// </summary>
+            /// <param name="list"></param>
+            /// <param name="fullTypeName">是否使用类型全名存储</param>
+            /// <param name="formatting">格式化</param>
+            /// <returns>json文本</returns>
+            //public static string SerializePolyArray(IList list, bool fullTypeName = false, Formatting formatting = Formatting.None)
+            //{
+            //    JArray jArray = new JArray();
+
+            //    foreach (var item in list)
+            //    {
+            //        string typeName = fullTypeName ? item.GetType().FullName : item.GetType().Name;
+            //        JObject jObject = new JObject
+            //    {
+            //        { "type", typeName },
+            //        { "data", JToken.FromObject(item) }
+            //    };
+            //        jArray.Add(jObject);
+            //    }
+            //    return jArray.ToString(formatting);
+            //}
+
+            ///// <summary>
+            ///// 反序列化多态数组
+            ///// </summary>
+            ///// <typeparam name="T">基类类型</typeparam>
+            ///// <param name="json">json文本</param>
+            ///// <param name="nameSpace">命名空间，(序列化时使用的是全名时传null即可)</param>
+            ///// <returns>多态数组</returns>
+            //public static T[] DeserializePolyArray<T>(string json, string nameSpace = null)
+            //{
+            //    JArray jArray = JArray.Parse(json);
+
+            //    List<T> values = new List<T>();
+
+            //    foreach (var jObject in jArray.Children<JObject>())
+            //    {
+            //        Type type;
+            //        if (string.IsNullOrEmpty(nameSpace))
+            //            type = Type.GetType(jObject.GetValue("type").Value<string>());
+            //        else
+            //            type = Type.GetType($"{nameSpace}.{jObject.GetValue("type").Value<string>()}");
+
+            //        var value = jObject.GetValue("data").ToObject(type);
+
+            //        values.Add((T)value);
+            //    }
+
+            //    return values.ToArray();
+            //}
+        }
+    }
+}
