5149fe7f3b2b3c179b35e432485e29fdbf8e9671
黄安恺 <huangankai@huayugames.com>
2026-04-08 17:27:24 +0800

    XDataTable

--- Runtime/Modules/Data/XDataTable.Manager.cs
+++ Runtime/Modules/Data/XDataTable.Manager.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using System.Reflection;
-using XFramework.Resource;
-using XFramework;
+using XFramework.Json;
 
 namespace XFramework.Data
 {
@@ -50,154 +46,12 @@ namespace XFramework.Data
     
     public partial class XDataTable
     {
-        private static readonly Dictionary<Type, Type> S_DataToTableType = new();
-        
-        private static readonly Dictionary<Type, IList> S_DataListMap = new();
-        private static readonly Dictionary<Type, IDictionary> S_DataDictMap = new();
-        private static readonly Dictionary<Type, IDictionary> S_DataAliasDictMap = new();
-        
-        static XDataTable()
-        {
-            var types = Utility.Reflection.GetGenericTypes(typeof(XDataTable<>), 1,"Assembly-CSharp", "XFrameworkRuntime");
-            
-            var map = new Dictionary<Type, Type>();
-
-            foreach (var tableType in types)
-            {
-                var targetDataTypeAttr = tableType.GetCustomAttribute<TargetDataType>(false);
-                if (targetDataTypeAttr?.targetType != null)
-                {
-                    var targetDataType = targetDataTypeAttr.targetType;
-                    if (!map.ContainsValue(targetDataType))
-                    {
-                        S_DataToTableType[targetDataType] = tableType;
-                    }
-                    else
-                    {
-                        var existtableType = map.First(kvp => kvp.Value == targetDataType).Key;
-                        Debug.LogError($"Data type {targetDataType.FullName} has repeated ScriptObject types, {existtableType.FullName}, {tableType.FullName}");
-                    }
-                }
-                else
-                {
-                    Debug.LogError($"Data type {tableType.FullName} missing TargetDataAttribute");
-                }
-            }
-        }
-        
-        public static IReadOnlyList<T> LoadData<T>() where T : IData
-        {
-            var dataType = typeof(T);
-            if (!S_DataListMap.ContainsKey(dataType))
-            {
-                if (!S_DataToTableType.TryGetValue(dataType, out var tableType))
-                {
-                    throw new Exception($"Data type {dataType.FullName} not registered");
-                }
-
-                var dataResourcePathAttr = tableType.GetCustomAttribute<DataResourcePath>(false);
-                if (dataResourcePathAttr == null)
-                {
-                    throw new Exception($"Data type {tableType.FullName} missing DataResourcePath attribute");
-                }
-                var textAsset = ResourceManager.Instance.Load<TextAsset>(dataResourcePathAttr.path);
-                if (textAsset == null)
-                {
-                    throw new Exception($"Data text asset missing at path: {dataResourcePathAttr.path}");
-                }
-                var so = textAsset.ToXTextAsset<XDataTable<T>>(tableType);
-                S_DataListMap[dataType] = so.items;
-            }
-            
-            var soItems = S_DataListMap[dataType];
-            return (IReadOnlyList<T>)soItems;
-        }
-
-        public static IReadOnlyDictionary<int, TValue> LoadDictData<TValue>() where TValue : IDataHasKey<int>
-        {
-            return LoadDictData<int, TValue>();
-        }
-
-        public static IReadOnlyDictionary<string, TValue> LoadDictDataStr<TValue>() where TValue : IDataHasKey<string>
-        {
-            return LoadDictData<string, TValue>();
-        }
-
-        public static IReadOnlyDictionary<TKey, TValue> LoadDictData<TKey, TValue>() where TValue : IDataHasKey<TKey>
-        {
-            var dataType = typeof(TValue);
-            if (!S_DataDictMap.ContainsKey(dataType))
-            {
-                var list = LoadData<TValue>();
-                var dict = new Dictionary<TKey, TValue>();
-                foreach (var item in list)
-                {
-                    if (!dict.TryAdd(item.PrimaryKey, item))
-                    {
-                        throw new Exception($"Data type {dataType.FullName} has repeated primary key {item.PrimaryKey}");
-                    }
-                }
-                S_DataDictMap[dataType] = dict;
-            }
-            
-            return (IReadOnlyDictionary<TKey, TValue>)S_DataDictMap[dataType];
-        }
-
-        public static TValue GetData<TValue>(int id) where TValue : IDataHasKey<int>
-        {
-            return GetData<int, TValue>(id);
-        }
-
-        public static TValue GetData<TKey, TValue>(TKey key) where TValue : IDataHasKey<TKey>
-        {
-            if (LoadDictData<TKey, TValue>().TryGetValue(key, out var value))
-            {
-                return value;
-            }
-            throw new Exception($"Data type {typeof(TValue).FullName} missing key {key}");
-        }
-
-        public static IReadOnlyDictionary<string, TValue> LoadAliasDictData<TValue>() where TValue : IDataHasAlias
-        {
-            var dataType = typeof(TValue);
-            if (S_DataAliasDictMap.TryGetValue(dataType, out var value))
-            {
-                return (IReadOnlyDictionary<string, TValue>)value;
-            }
-            
-            var list = LoadData<TValue>();
-            var dict = new Dictionary<string, TValue>();
-            foreach (var item in list)
-            {
-                if (string.IsNullOrEmpty(item.Alias))
-                {
-                    continue;
-                }
-                if (!dict.TryAdd(item.Alias, item))
-                {
-                    throw new Exception($"Data type {dataType.FullName} has repeated alias {item.Alias}");
-                }
-            }
-            S_DataAliasDictMap[dataType] = dict;
-            return dict;
-        }
-
-        public static TValue GetDataByAlias<TValue>(string alias) where TValue : IDataHasAlias
-        {
-            var dict = LoadAliasDictData<TValue>();
-            if (dict.TryGetValue(alias, out var value))
-            {
-                return value;
-            }
-            throw new Exception($"Data type {typeof(TValue).FullName} missing alias {alias}");
-        }
-
 #if UNITY_EDITOR
         [UnityEditor.MenuItem("XFramework/Data/Create Data Assets")]
         public static void CreateMissingDataAssets()
         {
-            XFramework.Json.XJson.SetUnityDefaultSetting();
-            var types = Utility.Reflection.GetGenericTypes(typeof(XDataTable<>), 1, "Assembly-CSharp", "XFrameworkRuntime");
+            XJson.SetUnityDefaultSetting();
+            var types = Utility.Reflection.GetGenericTypes(typeof(XDataTable<>), 5, "Assembly-CSharp", "XFrameworkRuntime");
             var createdCount = 0;
             foreach (var tableType in types)
             {
