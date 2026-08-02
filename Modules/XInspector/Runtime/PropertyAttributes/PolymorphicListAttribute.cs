390ab707a0f9882b469e1a6ef9475f70364ea9db
xdedzl <xdedzl@163.com>
2026-08-02 23:57:00 +0800

    适配团结

--- Modules/XInspector/Runtime/PropertyAttributes/PolymorphicListAttribute.cs
+++ Modules/XInspector/Runtime/PropertyAttributes/PolymorphicListAttribute.cs
@@ -9,13 +9,12 @@ namespace XFramework
 
         public PolymorphicListAttribute(params Type[] types)
 #if UNITY_6000_0_OR_NEWER
-            // Unity 6 移除了 useForChildren 字段，需用构造
+            // Unity 6：移除了 useForChildren 字段，用构造
             : base(true)
 #endif
         {
 #if !UNITY_6000_0_OR_NEWER
-            // 团结引擎(基于2022.3)与2022.3 没有 base(true) 构造，改用字段赋值
-            useForChildren = true;
+            // 团结引擎：base(true) 与 useForChildren 均不支持，保持默认
 #endif
             this.types = types ?? Type.EmptyTypes;
         }
